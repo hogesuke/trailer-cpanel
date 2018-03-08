665fafb5e6f8e7f95f006f7481f6d537f4b72da1
hogesuke <miyado@gmail.com>
2018-03-08 22:17:55 +0900

    normalize.cssを導入, headerを追加

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,33 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div id="root">
+    <header id="header">
+    </header>
+    <div id="main">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import 'normalize.css'
 
 export default Vue.extend({
   name: 'App'
 })
 </script>
 
-<style>
-#app {
+<style lang="scss">
+#root {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+  #header {
+    height: 60px;
+    border-bottom: solid 1px #ccc;
+  }
 }
 </style>

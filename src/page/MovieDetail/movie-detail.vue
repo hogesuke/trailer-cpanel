fc8d61df1a67e3d8c01f33bc5b03e92fbae57696
hogesuke <miyado@gmail.com>
2018-03-12 22:23:26 +0900

    titleの表示を追加

--- src/page/MovieDetail/movie-detail.vue
+++ src/page/MovieDetail/movie-detail.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="trailer">
-    <iframe
-      width="1000"
-      height="562"
-      :src=movieURL
-      frameborder="0"
-      allow="autoplay; encrypted-media"
-      allowfullscreen>
-    </iframe>
+  <div class="movie">
+    <div class="trailer">
+      <iframe
+        width="1000"
+        height="562"
+        :src=movieURL
+        frameborder="0"
+        allow="autoplay; encrypted-media"
+        allowfullscreen>
+      </iframe>
+    </div>
+    <div class="description">
+      <div class="title">{{item.title}}</div>
+    </div>
   </div>
 </template>
 
@@ -15,8 +20,14 @@
 </script>
 
 <style scoped lang="scss">
-  .trailer {
-    display: flex;
-    justify-content: center;
+  .movie {
+    .trailer {
+      display: flex;
+      justify-content: center;
+    }
+
+    .description {
+      font-size: 24px;
+    }
   }
 </style>

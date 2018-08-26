d3622d5581313511a634b653028ceb2b3fd20125
hogesuke <miyado@gmail.com>
2018-08-27 00:30:45 +0900

    縦持ち横持ちでそれぞれ適用するstyleを変更するように修正

--- src/page/MovieDetail.vue
+++ src/page/MovieDetail.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="trailer">
-    <header>
-      <router-link to="/" class="logo">
-        <div class="img">
-          EXIT
+  <div class="trailer" :style="trailerStyle">
+    <header :style="headerStyle">
+      <router-link to="/" class="back-link">
+        <div class="exit-button">
+          <span>EXIT</span>
           <div class="overlay"></div>
         </div>
       </router-link>
@@ -30,13 +30,37 @@ export default {
   data () {
     return {
       item: null,
-      isHide: true
+      isHide: true,
+      isVertical: true,
+      count: 0
     }
   },
   methods: {
     ...mapMutations({
       setDark: types.SET_DARK
-    })
+    }),
+    addOrientationChangeEventListener () {
+      const ua = navigator.userAgent.toLowerCase()
+      const isiPhone = (ua.indexOf('iphone') > -1) // iPhone
+      const isiPad = (ua.indexOf('ipad') > -1) // iPad
+      const isAndroid = (ua.indexOf('android') > -1) && (ua.indexOf('mobile') > -1) // Android
+      const isAndroidTablet = (ua.indexOf('android') > -1) && (ua.indexOf('mobile') === -1) // Android Tablet
+
+      // iOS
+      if (isiPhone || isiPad) {
+        window.onorientationchange = this.handleOrientationChange
+      }
+      // Android
+      if (isAndroid || isAndroidTablet) {
+        window.onresize = this.handleOrientationChange
+      }
+    },
+    handleOrientationChange () {
+      // todo PCではwindow.orientationを使わない判定方法とするように修正する
+      const direction = Math.abs(window.orientation)
+      this.isVertical = direction !== 90
+      this.count++
+    }
   },
   computed: {
     ...mapState([
@@ -44,6 +68,12 @@ export default {
     ]),
     movieURL () {
       return this.item ? `https://www.youtube.com/embed/${this.item.trailers[0].videoId}?enablejsapi=1&autoplay=1` : ''
+    },
+    trailerStyle () {
+      return this.isVertical ? { paddingTop: '5rem' } : { padding: '5rem 5rem 0' }
+    },
+    headerStyle () {
+      return this.isVertical ? { paddingLeft: '1rem' } : { paddingLeft: '5rem' }
     }
   },
   created () {
@@ -61,9 +91,14 @@ export default {
         this.isHide = false
       }, 500)
     }, 800)
+
+    this.addOrientationChangeEventListener()
+    this.handleOrientationChange()
   },
   beforeDestroy () {
     clearTimeout(this.theaterModeTimeout)
+    window.onorientationchange = null
+    window.onresize = null
     this.setDark(false)
   }
 }
@@ -71,22 +106,17 @@ export default {
 
 <style scoped lang="scss">
   .trailer {
-    padding: 4rem 4rem 0;
-
     header {
       position: fixed;
       top: 0;
       left: 0;
       width: 100%;
-      padding: 1rem 4rem 0;
+      padding: 1rem 0;
 
-      .logo {
+      .back-link {
         position: relative;
-        font-size: 1.6rem;
-        letter-spacing: .02em;
-        color: #444;
 
-        .img {
+        .exit-button {
           $image-size: 3rem;
 
           position: relative;
@@ -97,6 +127,8 @@ export default {
           border-radius: 3px;
           text-align: center;
           line-height: $image-size;
+          letter-spacing: 0.05em;
+          font-size: 1.6rem;
           font-weight: bold;
           color: #269463;
           animation: lighting 0.8s ease 1.5s 1 normal both;
@@ -132,8 +164,8 @@ export default {
     }
 
     .wrapper {
-      // todo 参考: https://design.webclips.jp/youtube-movie-size/
-      // todo header分幅を狭めるようにする
+      // 参考: https://design.webclips.jp/youtube-movie-size/
+      // todo iPhoneXで横にした際に動画が入りきっていない
       position: relative;
       padding-bottom: 56.25%;
       height: 0;

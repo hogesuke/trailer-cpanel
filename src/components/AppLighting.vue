d25a3310b310d3b4a1a3592655b84b82c3c20408
hogesuke <miyado@gmail.com>
2018-09-10 15:10:38 +0900

    再生画面に遷移する際のアニメーションを行うコンポーネントを追加（途中）

---
+++ src/components/AppLighting.vue
@@ -0,0 +1,28 @@
+<template>
+  <transition name="router">
+    <div
+      v-if="isDark"
+      class="overlay" />
+  </transition>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+    }
+  },
+  computed: {
+    ...mapState([
+      'isDark'
+    ])
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .overlay {
+  }
+</style>

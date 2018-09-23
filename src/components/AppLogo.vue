9a74a6765165a935aac3461c21b50e43a1ab67eb
hogesuke <miyado@gmail.com>
2018-09-23 19:19:37 +0900

    filmを積み重ねるように修正

--- src/components/AppLogo.vue
+++ src/components/AppLogo.vue
@@ -1,13 +1,42 @@
 <template>
-  <div class="app-logo" />
+  <div class="app-logo">
+    <div class="film-svg film-1" />
+    <div class="film-svg film-2" />
+    <div class="film-svg film-3" />
+  </div>
 </template>
 
 <style scoped lang="scss">
   .app-logo {
-    width: 70px;
-    height: 60px;
-    margin-left: 10px;
-    background: url('../assets/film.svg') no-repeat;
-    background-size: 70px 60px;
+    position: relative;
+    width: 60px;
+
+    .film- {
+      &svg {
+        position: absolute;
+        width: 50px;
+        height: 40px;
+        margin-left: 10px;
+        background: url('../assets/film.svg') no-repeat;
+        background-size: 50px 40px;
+      }
+
+      &1 {
+        top: 0;
+        z-index: 10;
+      }
+
+      &2 {
+        top: 8px;
+        left: 6px;
+        z-index: 9;
+      }
+
+      &3 {
+        top: 16px;
+        left: 0;
+        z-index: 8;
+      }
+    }
   }
 </style>

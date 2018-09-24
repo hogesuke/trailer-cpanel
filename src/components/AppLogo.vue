c1b2217fce4e578c37997e9865728015692c6457
hogesuke <miyado@gmail.com>
2018-09-24 22:40:33 +0900

    重ねているfilmの下になる部分のsvgを別に作成

--- src/components/AppLogo.vue
+++ src/components/AppLogo.vue
@@ -17,25 +17,27 @@
         width: 50px;
         height: 40px;
         margin-left: 10px;
-        background: url('../assets/film.svg') no-repeat;
         background-size: 50px 40px;
       }
 
       &1 {
         top: 0;
         z-index: 10;
+        background: url('../assets/film.svg') no-repeat;
       }
 
       &2 {
         top: 8px;
         left: 6px;
         z-index: 9;
+        background: url('../assets/film-flat.svg') no-repeat;
       }
 
       &3 {
         top: 16px;
         left: 0;
         z-index: 8;
+        background: url('../assets/film-flat.svg') no-repeat;
       }
     }
   }

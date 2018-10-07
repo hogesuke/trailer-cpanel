22f200d53d7f85fd645dbf1f44e773fe1ed0b9f7
hogesuke <miyado@gmail.com>
2018-10-07 23:57:03 +0900

    movies取得のfunctionsを追加

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,33 @@
+const express = require('express');
 const functions = require('firebase-functions');
+const cors = require('cors');
+const admin = require('firebase-admin');
+const serviceAccount = require('./config.json');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const app = express();
+
+app.use(cors({ origin: true }));
+
+app.get('/movies', (req, res) => {
+    const db = admin.firestore();
+    return db.collection('movies').get()
+        .then(querySnapshot => {
+            const items = [];
+            querySnapshot.forEach(doc => {
+                const data = doc.data();
+                data.id = doc.id;
+                items.push(data)
+            });
+            res.set('Content-Type', 'application/json');
+            res.status(200).send(items);
+            return;
+        }).catch(err => {
+            res.status(500).send(err);
+        });
+});
+
+exports.trial = functions.https.onRequest(app);
